60f926844cdba687da7dc03a35b8d5a660526342
Antonia <welzel@student.chalmers.se>
2019-09-09 12:58:24 +0200

    game init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import de.gurkenlabs.litiengine.Game;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World! :)");
+
+        Game.init();
+        Game.start();
     }
 }
