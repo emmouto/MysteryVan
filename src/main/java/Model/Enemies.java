4265d80a333c70929320aedc0b0750210c7d9f5c
Jennifer Krogh <jennifer.krogh8@gmail.com>
2019-09-16 11:52:10 +0200

    Added stuff to enemy and equipment classes

--- src/main/java/Model/Enemies.java
+++ src/main/java/Model/Enemies.java
@@ -1,4 +1,23 @@
 package Model;
 
 public class Enemies {
+
+    private int HP;
+    private Equipment weapon;
+
+    public int getHP() {
+        return HP;
+    }
+
+    public Equipment getWeapon() {
+        return weapon;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public void setWeapon(Equipment weapon) {
+        this.weapon = weapon;
+    }
 }
