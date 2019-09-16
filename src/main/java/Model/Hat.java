28870923fbc5fb332ca962f32dad810c241d07e2
Jennifer Krogh <jennifer.krogh8@gmail.com>
2019-09-16 11:47:12 +0200

    Added attributes to hat class

--- src/main/java/Model/Hat.java
+++ src/main/java/Model/Hat.java
@@ -1,4 +1,14 @@
 package Model;
 
 public class Hat {
+
+    private Boost boost;
+
+    public Boost getBoost() {
+        return boost;
+    }
+
+    public void setBoost(Boost boost) {
+        this.boost = boost;
+    }
 }
