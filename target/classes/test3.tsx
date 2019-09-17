8c6f6cee38a13c7da2f1f8d6ecc13a056d100213
AdamRohdell <rohdell@student.chalmers.se>
2019-09-17 10:51:52 +0200

    fixed GameRunner class

---
+++ target/classes/test3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="test3" tilewidth="16" tileheight="16" tilecount="792" columns="36">
+ <image source="../../../../../../../Downloads/Sunny-land-woods-files/tileset.png" width="576" height="352"/>
+</tileset>
