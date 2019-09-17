9ed1d663c4a7a9c08323a49620033dc9152740b9
Antonia <welzel@student.chalmers.se>
2019-09-17 11:13:00 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -3,15 +3,22 @@ import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.GameListener;
 import de.gurkenlabs.litiengine.environment.Environment;
 import de.gurkenlabs.litiengine.environment.tilemap.*;
+<<<<<<< Updated upstream
 import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 import de.gurkenlabs.litiengine.resources.Resource;
 import de.gurkenlabs.litiengine.resources.Resources;
+=======
+>>>>>>> Stashed changes
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 public class GameRunner {
 
@@ -20,11 +27,14 @@ public class GameRunner {
         Game.setInfo("gameinfo.xml");
         Game.init(args);
 
+<<<<<<< Updated upstream
 
         Game.screens().add(new GameScreen());
 
         Game.world().loadEnvironment(new Environment("src/main/resources/map2.tmx"));
 
+=======
+>>>>>>> Stashed changes
         Game.start();
 
 
