72fc309af9756c18e4d4708ddb88f0df42931c46
AdamRohdell <rohdell@student.chalmers.se>
2019-09-17 11:57:09 +0200

    Resized window and configured game to support map and resolution

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,9 +1,11 @@
+import Controller.MapController;
 import View.MenyView;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.GameListener;
 import de.gurkenlabs.litiengine.environment.Environment;
 import de.gurkenlabs.litiengine.environment.tilemap.*;
 import de.gurkenlabs.litiengine.gui.screens.GameScreen;
+import de.gurkenlabs.litiengine.gui.screens.Resolution;
 import de.gurkenlabs.litiengine.resources.Resource;
 import de.gurkenlabs.litiengine.resources.Resources;
 
@@ -16,14 +18,31 @@ import java.util.Map;
 public class GameRunner {
 
     public static void main(String[] args) {
+        MapController mc = new MapController();
+
+
+        Game.config().graphics().setResolutionHeight(720);
+        Game.config().graphics().setFullscreen(true);
+        Game.config().graphics().setResolutionWidth(1280);
 
         Game.setInfo("gameinfo.xml");
+
         Game.init(args);
+        Game.window().setResolution(Resolution.custom(1280, 720, "720p"));
+
 
 
         Game.screens().add(new GameScreen());
 
         Game.world().loadEnvironment(new Environment("src/main/resources/new_map.tmx"));
+
+
+        mc.initCamera();
+
+
+
+
+
 
         Game.start();
 
