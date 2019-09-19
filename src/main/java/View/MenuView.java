090e1cad86dafdaf8470d3443290c838b694a184
Emma <emmp@student.chalmers.se>
2019-09-19 11:08:39 +0200

    Testing stuff

--- src/main/java/View/MenuView.java
+++ src/main/java/View/MenuView.java
@@ -1,10 +1,12 @@
 package View;
 
 import de.gurkenlabs.litiengine.graphics.TextRenderer;
+import de.gurkenlabs.litiengine.gui.ImageComponent;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
 import de.gurkenlabs.litiengine.resources.Resources;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MenuView extends Screen {
     public MenuView(String screenName) {
@@ -14,8 +16,8 @@ public class MenuView extends Screen {
     @Override
     public void render(final Graphics2D g) {
         super.render(g);
-        g.setFont(Resources.fonts().get("src/main/resources/fonts/Pixeled.ttf",32f));
-        g.setColor(Color.RED);
-        TextRenderer.render(g, "Test text", 100, 100);
+        g.setFont(Resources.fonts().get("src/main/resources/fonts/Pixeled.ttf",64f));
+        g.setColor(Color.WHITE);
+        TextRenderer.render(g, "CHALMERSFORCE", 224, 360);
     }
 }
