659437fa4b98cc628aa58ddd7b2beff8c3dd73f4
Jennifer Krogh <jennifer.krogh8@gmail.com>
2019-09-23 15:44:44 +0200

    Highscore class in model + defeat view

---
+++ src/main/java/View/DefeatView.java
@@ -0,0 +1,10 @@
+package View;
+
+import de.gurkenlabs.litiengine.gui.screens.Screen;
+
+public class DefeatView extends Screen {
+    public DefeatView(String screenName) {
+        super(screenName);
+    }
+}
+
