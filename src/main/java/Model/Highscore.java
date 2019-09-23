d2f22d6c750d744a70a29b0689a5358535171bd7
Jennifer Krogh <jennifer.krogh8@gmail.com>
2019-09-23 15:37:32 +0200

    Highscore class in model

---
+++ src/main/java/Model/Highscore.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Highscore {
+
+    private int highscore;
+
+    public int getHighscore() {
+        return highscore;
+    }
+
+    public void setHighscore(int highscore) {
+        this.highscore = highscore;
+    }
+}
