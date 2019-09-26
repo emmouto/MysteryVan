03b40f9016c5bad48bb199de3e6cc8eef7a4b310
Adam <rohdell@student.chalmers.se>
2019-09-26 02:33:45 +0200

    tried to fix spawning enemies, failed

--- src/main/java/Model/Enemy.java
+++ src/main/java/Model/Enemy.java
@@ -3,34 +3,51 @@ package Model;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.IUpdateable;
+import de.gurkenlabs.litiengine.annotation.AnimationInfo;
 import de.gurkenlabs.litiengine.annotation.MovementInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
+import de.gurkenlabs.litiengine.graphics.IRenderable;
+import de.gurkenlabs.litiengine.graphics.ImageRenderer;
+import de.gurkenlabs.litiengine.graphics.RenderEngine;
+import de.gurkenlabs.litiengine.graphics.RenderType;
 import de.gurkenlabs.litiengine.pathfinding.Path;
 import de.gurkenlabs.litiengine.physics.MovementController;
+import de.gurkenlabs.litiengine.resources.Resources;
 
+import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
 
 @MovementInfo(velocity = 30)
-public class Enemy extends Creature implements IUpdateable, ICollidable, IMovable {
+@AnimationInfo(spritePrefix = "enemy")
+public class Enemy extends Creature implements IUpdateable, ICollidable, IMovable, IRenderable{
 
     private int HP;
     private Equipment equipment; //Enemy can have a weapon, armor etc that will make them harder to defeat.
     private Path path;
-    private Point2D currentDestination;
+    private int currentDestinationIndex = 0;
     private MovementController<Enemy> mv;
+    private BufferedImage sprite;
+    private Point2D spawn;
 
     public Enemy(){
-        this.mv = new MovementController<Enemy>(this);
     }
 
     public void updatePath (Path p){
         this.path = p;
+        this.setMoveDestination(null);
+        currentDestinationIndex = 0;
     }
 
     private void followPath(){
-        this.setMoveDestination(path.getStart());
-        while(this.getLocation().distance(this.getMoveDestination()) >= 10){
+        if (this.getMoveDestination() == null){
+            this.setMoveDestination(path.getStart());
+        }
+        if(this.getLocation().distance(this.getMoveDestination()) >= 10){
             move();
+        } else {
+            currentDestinationIndex++;
+            this.setMoveDestination(path.getPoints().get(currentDestinationIndex));
         }
     }
 
@@ -56,6 +73,11 @@ public class Enemy extends Creature implements IUpdateable, ICollidable, IMovabl
 
     @Override
     public void update() {
-        
+       // followPath();
+    }
+
+    @Override
+    public void render(Graphics2D graphics2D) {
+        graphics2D.drawImage(sprite, 640, 360, sprite.getWidth(),sprite.getHeight(), null);
     }
 }
