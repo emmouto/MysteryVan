0cbb003ee9aa1f25a08b85f997308bed4d666d98
AdamRohdell <rohdell@student.chalmers.se>
2019-10-10 15:57:59 +0200

    Added generated test-classes and methods, need to be filled in ofcourse

---
+++ test/Model/ColliderTest.java
@@ -0,0 +1,20 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ColliderTest {
+
+    @Test
+    public void updatePosition() {
+    }
+
+    @Test
+    public void updateSize() {
+    }
+
+    @Test
+    public void isColliding() {
+    }
+}
---
+++ test/Model/EnemyTest.java
@@ -0,0 +1,24 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EnemyTest {
+
+    @Test
+    public void checkGrounded() {
+    }
+
+    @Test
+    public void checkPlayerCollision() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void move() {
+    }
+}
